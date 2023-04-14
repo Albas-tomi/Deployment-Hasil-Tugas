5cbcf06b4d95ae6a7172e380b6e6d6b568b0d1f8
Muhammad Albas Tomi <albustomi679@gmail.com>
2023-04-14 13:59:42 +0700

    update

--- src/components/Home/ItemSearch.jsx
+++ src/components/Home/ItemSearch.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ItemSearch() {
   return (
-      <button className="bg-white rounded-full px-5 py-1 mr-2 mt-4 hover:bg-slate-400 duration-200 ">Samsung</button>
+      <button className="bg-white rounded-full px-5 py-1 mr-2 mt-4 hover:bg-slate-400 duration-200 ">Bakso</button>
   );
 }
 
--- src/components/Home/SearchHero.jsx
+++ src/components/Home/SearchHero.jsx
@@ -14,11 +14,11 @@ function SearchHero() {
     <div className="jumbroton flex items-center justify-center w-full bg-[#B8B5FF] py-10 px-3">
       <div className="w-full sm:w-4/5 md:w-3/5 xl:w-1/2">
         <h1 className="text-[25px] font-bold text-center">
-          The place where all the css animation <br></br> designs in the world
+          The place where all Resto <br></br>  in the Malang City
         </h1>
         <h2 className="text-[14px] text-center font-light pt-3">
-          Many expert developers have joined CoderAnimate <br></br> so find
-          thousands of designs here
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br></br> so find
+          thousands of resto here
         </h2>
         <Search value={query} onChange={onChangeHandler} />
         <LatestSearch />
